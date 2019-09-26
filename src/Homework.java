a2c93a0c0f97656efecbdb02bed6f7de9f588493
Karol <zuralskikarol@gmail.com>
2019-09-26 17:08:45 +0200

    [HW03] - Dodatkowe zmiany w kodzie klasy

--- src/Homework.java
+++ src/Homework.java
@@ -1,9 +1,17 @@
 public class Homework {
     public static void main(String[] args) {
+        System.out.println("--------------------");
         System.out.println("Uczę");
         System.out.println("się");
         System.out.println("programowania");
         System.out.println("i");
         System.out.println("commitowania");
+        System.out.println("--------------------");
+        System.out.println("Dodatkowe zmiany w kodzie!");
+        System.out.println(3);
+        System.out.println(2);
+        System.out.println(1);
+        System.out.println('?');
+        System.out.println("--------------------");
     }
 }
